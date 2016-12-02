3e9f79ed383dae4b4369fbf21515378695b5ad28
TeChengHung <dan12411@gmail.com>
2016-12-02 14:23:50 +0800

    first push

---
+++ iOSFinalAssessmentQ4/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  iOSFinalAssessmentQ4
+//
+//  Created by 洪德晟 on 2016/12/2.
+//  Copyright © 2016年 洪德晟. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
